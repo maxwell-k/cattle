64d5fd0da6e724e234dbd05e88f83aa7d62d4526
Keith Maxwell <keith.maxwell@gmail.com>
2017-07-29 23:55:39 +0000

    Add clipboard functions

---
+++ chroot/etc/profile.d/osc52.sh
@@ -0,0 +1,29 @@
+#/bin/sh
+# ChromeOS clipboard functions
+#
+# -   Use OSC 52 escape sequence
+# -   Based upon https://github.com/chromium/hterm/blob/master/etc/osc52.vim
+# -   \e and \033 and \x1b are all equivalent
+# -   \x07 is the string terminator:
+#     http://invisible-island.net/xterm/xterm.faq.html
+
+osc52() {
+	# Copy to the ChromeOS clipboard
+	#
+	# Usage:
+	#     osc52 "argument"
+	#     echo "stdin" | osc52
+	#
+	# relies on base64 being available e.g. via busybox
+	if test -t 0 ; then
+		printf "\x1b]52;c;%s\x07" $(printf "$1" | base64)
+	else
+		printf "\x1b]52;c;%s\x07" "$(base64)"
+	fi
+}
+
+yy() {
+	# Copy the last command to the ChromeOS clipboard
+	sed -n '$!h;$g;$p' $HISTFILE |
+	osc52
+}
