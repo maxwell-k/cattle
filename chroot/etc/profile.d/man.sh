c1c5455ae04e672ceaedce2a6939d58c42402e1c
Keith Maxwell <keith.maxwell@gmail.com>
2017-10-28 22:23:42 +0100

    Exit man if page does not exist

--- chroot/etc/profile.d/man.sh
+++ chroot/etc/profile.d/man.sh
@@ -1,7 +1,9 @@
 test -f /usr/local/share/man/mandoc.db || sudo makewhatis
 man() {
-	# portable and doesn't have version information in file paths
-	PAGER="cat" /usr/bin/man $@ | \
-	MAN_PN=1 vim -M "+runtime ftplugin/man.vim" +MANPAGER -
+	if /usr/bin/man $@ > /dev/null ; then
+		# portable and doesn't have version information in file paths
+		PAGER="cat" /usr/bin/man $@ | \
+		MAN_PN=1 vim -M "+runtime ftplugin/man.vim" +MANPAGER -
+	fi
 }
 test "$0" = /etc/profile.d/man.sh && man "$@"
