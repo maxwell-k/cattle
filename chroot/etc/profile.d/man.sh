273622eea1863221fda2078791eadd5e060aba0b
Keith Maxwell <keith.maxwell@gmail.com>
2017-12-29 09:01:49 +0000

    Simplify man pager

--- chroot/etc/profile.d/man.sh
+++ chroot/etc/profile.d/man.sh
@@ -1,14 +1,2 @@
 #/bin/sh
-# Also executed from:
-#   git config man.viewer
-#   git config man.MANPAGER.cmd
 test -f /usr/share/man/mandoc.db || sudo makewhatis
-man() {
-	# stderr is not affected so for example, man with no arguments will
-	# result in a usage message
-	if /usr/bin/man $@ > /dev/null ; then
-		# portable and doesn't have version information in file paths
-		PAGER="cat" /usr/bin/man $@ | MAN_PN=1 vim -M +MANPAGER -
-	fi
-}
-test "$0" = /etc/profile.d/man.sh && man "$@"
